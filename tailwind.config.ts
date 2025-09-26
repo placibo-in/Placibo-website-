8b6917266e3c35000c506bd0e076a673144163c8
avenauche <avenauchejeyasooriya@gmail.com>
2025-09-26 16:08:16 +0530

    3xl screen size extended in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,10 +15,12 @@ export default {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
-        "3xl": "1920px",
       },
     },
     extend: {
+      screens: {
+        "3xl": "1920px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
